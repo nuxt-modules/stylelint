1b21e32350dccd18952a660b4100a9ddb672d0d3
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2024-03-23 12:39:41 -0300

    fix: usage plugin by builder

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,6 @@
 import { defineNuxtModule, addVitePlugin, addWebpackPlugin, useLogger } from '@nuxt/kit'
 import type { StylelintPluginUserOptions as VitePlugin } from 'vite-plugin-stylelint'
 import type { Options as WebpackPlugin } from 'stylelint-webpack-plugin'
-import vitePluginStylelint from 'vite-plugin-stylelint'
-import StylelintWebpackPlugin from 'stylelint-webpack-plugin'
 import { relative } from 'pathe'
 import { watch } from 'chokidar'
 import { name, version } from '../package.json'
@@ -33,7 +31,7 @@ export default defineNuxtModule<ModuleOptions>({
     failOnWarning: false,
     failOnError: true
   }),
-  setup (options, nuxt) {
+  async setup (options, nuxt) {
     if (!nuxt.options.dev) {
       return
     }
@@ -58,18 +56,30 @@ export default defineNuxtModule<ModuleOptions>({
       nuxt.hook('close', () => watcher.close())
     }
 
-    addVitePlugin(vitePluginStylelint(options), { server: false })
+    if (nuxt.options.builder === '@nuxt/vite-builder') {
+      const vitePluginStylelint = await import('vite-plugin-stylelint').then(m => 'default' in m ? m.default : m)
 
-    const webpackOptions = {
-      ...options,
-      context: nuxt.options.srcDir,
-      files: options.include,
-      lintDirtyModulesOnly: !options.lintOnStart
+      addVitePlugin(() => {
+        return vitePluginStylelint(options)
+      }, { server: false })
+    } else if (nuxt.options.builder === '@nuxt/webpack-builder') {
+      const StylelintWebpackPlugin = await import('stylelint-webpack-plugin').then(m => 'default' in m ? m.default : m)
+
+      addWebpackPlugin(() => {
+        const webpackOptions = {
+          ...options,
+          context: nuxt.options.srcDir,
+          files: options.include,
+          lintDirtyModulesOnly: !options.lintOnStart
+        }
+
+        delete webpackOptions.include
+        delete webpackOptions.lintOnStart
+
+        return new StylelintWebpackPlugin(webpackOptions)
+      }, { server: false })
+    } else {
+      logger.warn('Unsupported builder ' + nuxt.options.builder + ', Stylelint is not enabled.')
     }
-
-    delete webpackOptions.include
-    delete webpackOptions.lintOnStart
-
-    addWebpackPlugin(new StylelintWebpackPlugin(webpackOptions), { server: false })
   }
 })
